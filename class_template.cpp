aad1babe4b2a88682ffc44770e2f0e376d106027
Yogesh <pantyogesh992@gmail.com>
2025-01-09 17:35:17 +0530

    Add files via upload

---
+++ class_template.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+template <class T>
+class number
+{
+    private :
+    T num;
+    public :
+    Number(Tn){
+        num =n ;
+    }
+    T get Num()
+    {
+        return num;
+    }
+};
+int main ()
+{
+    
+}
