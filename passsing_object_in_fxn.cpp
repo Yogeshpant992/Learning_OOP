41a1076fc146bf86555fcbaed44ada7e226f95d9
Yogeshpant992 <pantyogesh992@gmail.com>
2025-01-12 20:12:49 +0530

    Passing object

--- passsing_object_in_fxn.cpp
+++ passsing_object_in_fxn.cpp
@@ -4,22 +4,25 @@ class MYclass
 {
     public :
     int a ;
-    void myfunction(MYclass obj)
+    int myfunction(MYclass obj)
     {
         a = a * obj.a;
     }
 };
 int main ()
 {
-   MYclass E1,E2;
+   MYclass E1,E2,E3;
    E1.a = 23;
    E2.a = 42;
+   E3.a = 10;
 
    cout<<"inital Value "<<E1.a<<" "<<E2.a<<endl;
 
    E1.myfunction(E2);
 
-   cout<<"New value "<<E1.a<<" "<<E2.a<<endl;
 
+   E3.a= E1.a;
+
+   cout<<"Multiplication value "<<E3.a<<endl;
    return 0;
 }
