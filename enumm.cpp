ebe7e458b3a5983cb431f120b495d81447d1f1bb
Yogeshpant992 <pantyogesh992@gmail.com>
2025-01-17 22:40:54 +0530

    Enum class code

---
+++ enumm.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+enum data
+{
+    Avinash = 20,
+    rakesh = 35,
+    janam = 22,
+    hiten = 98,
+};
+int main()
+{
+    enum data obj;
+    obj=janam;
+    cout<<"The roll no. of the janam is :"<<janam<<endl;
+
+}
